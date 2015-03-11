0e98b86c416b69fb6ed3a705604c9f674deae109
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-03-10 22:20:37 -0400

    implemented delete film and new view

--- www/js/controller/mainCtrl.js
+++ www/js/controller/mainCtrl.js
@@ -3,6 +3,13 @@ flix.controller('mainCtrl', ['flixService', '$scope', '$ionicModal', function(fl
 		$scope.films = res.data;
 	});
 
+	$scope.onItemDelete = function(film) {
+		$scope.films.splice($scope.films.indexOf(film), 1);
+		flixService.deleteMovie(film._id).then(function(res){
+			$scope.films = res.data;
+		});
+	}
+
 	// Create and load the Modal
 	  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
 	    $scope.taskModal = modal;
@@ -12,6 +19,39 @@ flix.controller('mainCtrl', ['flixService', '$scope', '$ionicModal', function(fl
 		   animation: 'slide-in-up'
 		});
 
+	  $scope.addFilm = function(title) {
+	  	var data, storeFilm;
+
+	  	storeFilm = 'title=' + $scope.temp.Title + 
+				'&year=' + convertToInt($scope.temp.Year) +
+				'&rated=' + $scope.temp.Rated + 
+				'&released=' + $scope.temp.Released +
+				'&runtime=' + convertToInt($scope.temp.Runtime) +
+				'&genre=' + convertToArray($scope.temp.Genre) +
+				'&director=' + $scope.temp.Director + 
+				'&write=' + convertToArray($scope.temp.Writer) +
+				'&actors=' + convertToArray($scope.temp.Actors) + 
+				'&plot=' + $scope.temp.Plot +
+				'&language=' + convertToArray($scope.temp.Language) + 
+				'&country=' + $scope.temp.Country +
+				'&awards=' + $scope.temp.Awards + 
+				'&poster=' + $scope.temp.Poster +
+				'&metascore=' + convertToInt($scope.temp.Metascore) +
+				'&imdbRating=' + parseFloat($scope.temp.imdbRating) +
+				'&imdbVotes=' + convertToInt($scope.temp.imdbVotes) +
+				'&imdbID=' + $scope.temp.imdbID +
+				'&response=' + $scope.temp.Response;
+
+		flixService.saveMovie(storeFilm);
+
+		flixService.getMovie(true).then(function(res) {
+			$scope.films = res.data;
+		});
+
+		$scope.taskModal.hide();
+
+	  };
+
 	  // Called when the form is submitted
 	  $scope.searchMovie = function (title) {
 	  	var data, storeFilm;
@@ -19,34 +59,7 @@ flix.controller('mainCtrl', ['flixService', '$scope', '$ionicModal', function(fl
 			data = '?t=' + title.replace(" ", '%20');
 
 			flixService.getMovie(false, data).then(function (res) {
-				storeFilm = 'title=' + res.data.Title + 
-				'&year=' + convertToInt(res.data.Year) +
-				'&rated=' + res.data.Rated + 
-				'&released=' + res.data.Released +
-				'&runtime=' + convertToInt(res.data.Runtime) +
-				'&genre=' + convertToArray(res.data.Genre) +
-				'&director=' + res.data.Director + 
-				'&write=' + convertToArray(res.data.Writer) +
-				'&actors=' + convertToArray(res.data.Actors) + 
-				'&plot=' + res.data.Plot +
-				'&language=' + convertToArray(res.data.Language) + 
-				'&country=' + res.data.Country +
-				'&awards=' + res.data.Awards + 
-				'&poster=' + res.data.Poster +
-				'&metascore=' + convertToInt(res.data.Metascore) +
-				'&imdbRating=' + parseFloat(res.data.imdbRating) +
-				'&imdbVotes=' + convertToInt(res.data.imdbVotes) +
-				'&imdbID=' + res.data.imdbID +
-				'&response=' + res.data.Response;
-
-				flixService.saveMovie(storeFilm);
-
-				flixService.getMovie(true).then(function(res) {
-					$scope.films = res.data;
-				});
-
-				$scope.taskModal.hide();
-
+				$scope.temp = res.data;
 			});
 
 		};
